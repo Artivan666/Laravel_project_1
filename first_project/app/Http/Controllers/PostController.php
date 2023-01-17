f107e39a1ddc93f34d4467e1abc24e102e0a0787
Artivan666 <artivan667@gmail.com>
2023-01-17 10:50:00 +0300

    lesson #14

--- first_project/app/Http/Controllers/PostController.php
+++ first_project/app/Http/Controllers/PostController.php
@@ -8,31 +8,31 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $post = Post::where('is_published', 0)->first();
-       dump($post->title);
-        dd('end');
+       $posts = Post::all();
+       return view('posts', compact('posts'));
+        dd($posts);
     }
 
     public function create() {
         $postsArr = [
             [
-                'title' => 'Title 4',
-                'content' => 'Post 4',
-                'image' => 'Image 4',
+                'title' => 'Title 1',
+                'content' => 'Post 1',
+                'image' => 'Image 1',
                 'likes' => '85',
                 'is_published' => '1'
             ],
             [
-                'title' => 'Title 5',
-                'content' => 'Post 5',
-                'image' => 'Image 5',
+                'title' => 'Title 2',
+                'content' => 'Post 2',
+                'image' => 'Image 2',
                 'likes' => '34',
                 'is_published' => '1'
             ],
             [
-                'title' => 'Title 6',
-                'content' => 'Post 6',
-                'image' => 'Image 6',
+                'title' => 'Title 3',
+                'content' => 'Post 3',
+                'image' => 'Image 3',
                 'likes' => '327',
                 'is_published' => '0'
             ],
